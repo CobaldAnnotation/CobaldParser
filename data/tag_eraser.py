b0ae1c8871d987306955c631d466b8bbaa09a957
houcha <baiuk.is@phystech.edu>
2024-06-06 08:47:27 +0000

    Add more customization to tag_eraser.py script

--- data/tag_eraser.py
+++ data/tag_eraser.py
@@ -12,7 +12,9 @@ from common.sentence import Sentence
 def main(
     input_file_path: str,
     output_file_path: str,
-    keep_syntax: bool
+    erase_morphosyntax: bool,
+    erase_semslot: bool,
+    erase_semclass: bool
 ) -> None:
 
     with open(input_file_path, "r") as file:
@@ -20,19 +22,19 @@ def main(
 
     for sentence in tqdm(sentences):
         for token in sentence:
-            token.lemma = None
-            token.upos = None
-            token.xpos = None
-            token.feats = None
-
-            if not keep_syntax:
+            if erase_morphosyntax:
+                token.lemma = None
+                token.upos = None
+                token.xpos = None
+                token.feats = None
                 token.head = None
                 token.deprel = None
                 token.deps = None
-
-            token.misc = None
-            token.semslot = None
-            token.semclass = None
+                token.misc = None
+            if erase_semslot:
+                token.semslot = None
+            if erase_semclass:
+                token.semclass = None
     write_conllu(output_file_path, sentences)
 
 
@@ -51,11 +53,18 @@ if __name__ == "__main__":
         help='Output conllu file with tags removed.'
     )
     parser.add_argument(
-        '--keep-syntax',
-        action='store_true',
-        help='If set, syntax tags (head, deprel and deps) are preserved.'
+        '--erase-morphosyntax',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--erase-semslot',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--erase-semclass',
+        action='store_true'
     )
     args = parser.parse_args()
 
-    main(args.input_file, args.output_file, args.keep_syntax)
+    main(args.input_file, args.output_file, args.erase_morphosyntax, args.erase_semslot, args.erase_semclass)
 

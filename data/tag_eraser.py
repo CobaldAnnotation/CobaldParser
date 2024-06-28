f687ea6bbadf07418add5e65bb1185b58a682480
houcha <baiuk.is@phystech.edu>
2024-06-28 12:31:02 +0300

    Fix tag eraser script: filter out nulls and range tokens

--- data/tag_eraser.py
+++ data/tag_eraser.py
@@ -5,8 +5,8 @@ import argparse
 from tqdm import tqdm
 
 sys.path.append("..") # import 'common' package
-from common.parse_conllu import parse_conllu, write_conllu
-from common.sentence import Sentence
+from common.parse_conllu import parse_conllu_raw, write_conllu
+from common.sentence import Token, Sentence
 
 
 def main(
@@ -18,24 +18,32 @@ def main(
 ) -> None:
 
     with open(input_file_path, "r") as file:
-        sentences = parse_conllu(file)
+        token_lists = parse_conllu_raw(file)
 
-    for sentence in tqdm(sentences):
-        for token in sentence:
+    clean_sentences = []
+    for token_list in tqdm(token_lists):
+        clean_tokens = []
+        for token in token_list:
+            if token["form"] == "#NULL" or '-' in token["id"]:
+                continue
             if erase_morphosyntax:
-                token.lemma = None
-                token.upos = None
-                token.xpos = None
-                token.feats = None
-                token.head = None
-                token.deprel = None
-                token.deps = None
-                token.misc = None
+                token["lemma"] = None
+                token["upos"] = None
+                token["xpos"] = None
+                token["feats"] = None
+                token["head"] = None
+                token["deprel"] = None
+                token["deps"] = None
+                token["misc"] = None
             if erase_semslot:
-                token.semslot = None
+                token["semslot"] = None
             if erase_semclass:
-                token.semclass = None
-    write_conllu(output_file_path, sentences)
+                token["semclass"] = None
+            # Skip range tokens.
+            clean_tokens.append(Token(**token))
+        clean_sentences.append(Sentence(clean_tokens, token_list.metadata))
+
+    write_conllu(output_file_path, clean_sentences)
 
 
 if __name__ == "__main__":

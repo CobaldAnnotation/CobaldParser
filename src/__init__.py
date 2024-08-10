d97e7944b9f59bcb8ff62a500be7698ced56bb82
Ilia <bail@bail-23.10>
2024-08-10 17:05:48 +0200

    Fix logging

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 import logging
 
+# Ensure the standard Python logger is used instead of AllenNLP’s custom logger
+logging.setLoggerClass(logging.Logger)
+
 logger = logging.getLogger("parser")
 logger.propagate = False
 
 fileHandler = logging.FileHandler('parser.log', 'w')
-fileHandler.setLevel(logging.INFO)
 logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)

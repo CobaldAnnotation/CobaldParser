5b448ca6a49984d4179f7fc7c364151079cfbea5
houcha <ilia.baiuk.hu@gmail.com>
2024-12-12 17:27:38 +0100

    Simplify collate_fn

--- src/dataset.py
+++ src/dataset.py
@@ -4,11 +4,10 @@ from torch.utils.data import Dataset
 
 from src.read_conllu import read_conllu
 from src.processing import preprocess
-from src.utils import collect_values, pad_sequences, pad_matrices
+from src.utils import pad_sequences, pad_matrices
 
 
 class CobaldJointDataset(Dataset):
-
     def __init__(self, conllu_path: str, transform = None):
         super().__init__()
 
@@ -32,30 +31,21 @@ class CobaldJointDataset(Dataset):
     def collate_fn(samples: list[dict[str, any]], padding_value: int = -1) -> dict[str, any]:
         """Collate function for dataloader."""
 
-        # Define shortcut functions.
-        has_field = lambda field: all(field in sample for sample in samples)
+        result = {"words": [sample["words"] for sample in samples]}
 
-        result = {"words": collect_values(samples, "words")}
-
-        if has_field("lemma_rules"):
-            result["lemma_rules"] = pad_sequences(collect_values(samples, "lemma_rules"), padding_value)
-
-        if has_field("joint_pos_feats"):
-            result["joint_pos_feats"] = pad_sequences(collect_values(samples, "joint_pos_feats"), padding_value)
-
-        if has_field("deps_ud"):
-            result["deps_ud"] = pad_matrices(collect_values(samples, "deps_ud"), padding_value)
-
-        if has_field("deps_eud"):
-            result["deps_eud"] = pad_matrices(collect_values(samples, "deps_eud"), padding_value)
-
-        if has_field("miscs"):
-            result["miscs"] = pad_sequences(collect_values(samples, "miscs"), padding_value)
-
-        if has_field("deepslots"):
-            result["deepslots"] = pad_sequences(collect_values(samples, "deepslots"), padding_value)
-
-        if has_field("semclasses"):
-            result["semclasses"] = pad_sequences(collect_values(samples, "semclasses"), padding_value)
+        field_to_padding_fn_map = [
+            ("lemma_rules", pad_sequences),
+            ("joint_pos_feats", pad_sequences),
+            ("deps_ud", pad_matrices),
+            ("deps_eud", pad_matrices),
+            ("miscs", pad_sequences),
+            ("deepslots", pad_sequences),
+            ("semclasses", pad_sequences)
+        ]
+        for field, padding_fn in field_to_padding_fn_map:
+            # Pad field if it is present in samples.
+            if all(field in sample for sample in samples):
+                field_values = [sample[field] for sample in samples]
+                result[field] = padding_fn(field_values, padding_value)
 
         return result

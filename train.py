3b5f3fa90ec47c131ae835a9b609b86e2860dddb
houcha <ilia.baiuk.hu@gmail.com>
2025-04-15 17:10:24 +0200

    Customize model card: override Trainer's create_model_card

--- train.py
+++ train.py
@@ -1,13 +1,91 @@
+import os
+from typing import override
+
 from datasets import load_dataset, concatenate_datasets, DatasetDict
-from transformers import (
-    HfArgumentParser,
-    TrainingArguments,
-    Trainer
-)
+from transformers import HfArgumentParser, TrainingArguments, Trainer
+from transformers.modelcard import parse_log_history
+from huggingface_hub import ModelCard, ModelCardData, EvalResult
 
 from cobald_parser import CobaldParserConfig, CobaldParser
 from src.processing import preprocess, collate_with_padding
 from src.metrics import compute_metrics
+
+
+MODELCARD_TEMPLATE = """
+---
+{{ card_data }}
+---
+
+# Model Card for {{ model_name }}
+
+A transformer-based multihead parser for CoBaLD annotation.
+
+This model parses a pre-tokenized CoNLL-U text and jointly labels each token with three tiers of tags:
+* Grammatical tags (lemma, UPOS, XPOS, morphological features),
+* Syntactic tags (basic and enhanced Universal Dependencies),
+* Semantic tags (deep slot and semantic class).
+
+## Model Sources
+
+- **Repository:** https://github.com/CobaldAnnotation/CobaldParser
+- **Paper:** [coming soon]
+- **Demo:** [coming soon]
+
+## Citation
+
+[coming soon]
+"""
+
+
+class CustomTrainer(Trainer):
+    @override
+    def create_model_card(self, **kwargs):
+        """Create custom model card."""
+
+        dataset = self.eval_dataset
+        organization, model_name = self.hub_model_id.split('/')
+        hub_dataset_id = f"{organization}/{dataset.info.dataset_name}"
+
+        _, _, eval_results = parse_log_history(self.state.log_history)
+
+        def create_eval_result(metric_name: str, metric_type: str):
+            return EvalResult(
+                task_type='token-classification',
+                dataset_type=hub_dataset_id,
+                dataset_name=dataset.info.dataset_name,
+                dataset_split='validation',
+                metric_name=metric_name,
+                metric_type=metric_type,
+                metric_value=eval_results[metric_name]
+            )
+
+        card = ModelCard.from_template(
+            card_data=ModelCardData(
+                base_model=self.model.config.encoder_model_name,
+                datasets=hub_dataset_id,
+                language=dataset.info.config_name,
+                eval_results=[
+                    create_eval_result('Null F1', 'f1'),
+                    create_eval_result('Lemma F1', 'f1'),
+                    create_eval_result('Morphology F1', 'f1'),
+                    create_eval_result('Ud Jaccard', 'accuracy'),
+                    create_eval_result('Eud Jaccard', 'accuracy'),
+                    create_eval_result('Miscs F1', 'f1'),
+                    create_eval_result('Deepslot F1', 'f1'),
+                    create_eval_result('Semclass F1', 'f1')
+                ],
+                library_name='transformers',
+                license='gpl-3.0',
+                metrics=['accuracy', 'f1'],
+                model_name=self.hub_model_id,
+                pipeline_tag='token-classification',
+                tags=['pytorch']
+            ),
+            template_str=MODELCARD_TEMPLATE,
+            model_name=model_name
+        )
+        model_card_filepath = os.path.join(self.args.output_dir, "README.md")
+        card.save(model_card_filepath)
 
 
 def print_dataset_info(dataset_dict: DatasetDict):
@@ -47,11 +125,11 @@ if __name__ == "__main__":
     model = CobaldParser(model_config)
 
     # Create trainer and train the model.
-    trainer = Trainer(
+    trainer = CustomTrainer(
         model=model,
         args=training_args,
-        train_dataset=dataset_dict['train'],
-        eval_dataset=dataset_dict['validation'],
+        train_dataset=dataset_dict['train'].take(20),
+        eval_dataset=dataset_dict['validation'].take(100),
         data_collator=collate_with_padding,
         compute_metrics=compute_metrics
     )
